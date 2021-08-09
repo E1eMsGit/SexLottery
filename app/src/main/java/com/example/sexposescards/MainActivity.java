b456e35fa8755e037d553e8b0de20e4379bc274b
SAMsoftwareRep <simaginam@gmail.com>
2021-08-09 15:41:40 +0300

    111

--- app/src/main/java/com/example/sexposescards/MainActivity.java
+++ app/src/main/java/com/example/sexposescards/MainActivity.java
@@ -1,84 +1,38 @@
 package com.example.sexposescards;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
-import com.example.sexposescards.model.Pose;
-import com.example.sexposescards.model.PosesList;
-import com.example.sexposescards.model.Style;
-import com.example.sexposescards.model.StylesList;
-import com.wajahatkarim3.easyflipview.EasyFlipView;
+import com.example.sexposescards.interfaces.OnGameButtonClickListener;
+import com.example.sexposescards.interfaces.OnPreludeButtonClickListener;
 
-import java.util.List;
-import java.util.Random;
+public class MainActivity extends AppCompatActivity implements OnPreludeButtonClickListener, OnGameButtonClickListener {
 
-public class MainActivity extends AppCompatActivity {
-
-    private EasyFlipView styleEasyFlipView;
-    private ImageView styleImageView;
-    private TextView styleTextView;
-
-    private EasyFlipView poseEasyFlipView;
-    private ImageView poseImageView;
-    private TextView poseTextView;
-
-    private Button startButton;
-
-    private List<Style> stylesList;
-    private List<Pose> posesList;
-    private Random rnd;
+    private FragmentManager fragmentManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        init();
+        fragmentManager = getSupportFragmentManager();
 
-        startButton.setOnClickListener(view -> {
-            startButton.setEnabled(false);
-            styleEasyFlipView.flipTheView();
-            poseEasyFlipView.flipTheView();
-        });
+        if (savedInstanceState == null) {
+            fragmentManager.beginTransaction().add(R.id.container, new MenuFragment()).commit();
+        }
     }
 
-    private void init() {
-        setContentView(R.layout.fragment_game);
+    @Override
+    public void onPreludeButtonClicked() {
 
-        styleEasyFlipView = findViewById(R.id.style_easy_flip_view);
-        styleImageView = findViewById(R.id.style_image_view);
-        styleTextView = findViewById(R.id.style_text_view);
+    }
 
-        poseEasyFlipView = findViewById(R.id.pose_easy_flip_view);
-        poseImageView = findViewById(R.id.pose_image_view);
-        poseTextView = findViewById(R.id.pose_text_view);
-
-        startButton = findViewById(R.id.start_button);
-
-        stylesList = StylesList.getInstance(this).getStyles();
-        posesList = PosesList.getInstance(this).getPoses();
-        rnd = new Random();
-
-        styleEasyFlipView.setOnFlipListener((easyFlipView, newCurrentSide) -> {
-            if (newCurrentSide == EasyFlipView.FlipState.BACK_SIDE) {
-                Style style = stylesList.get(rnd.nextInt(stylesList.size()));
-                styleImageView.setImageResource(style.getImageId());
-                styleTextView.setText(style.getDescription());
-            } else {
-                startButton.setEnabled(true);
-            }
-        });
-        poseEasyFlipView.setOnFlipListener((easyFlipView, newCurrentSide) -> {
-            if (newCurrentSide == EasyFlipView.FlipState.BACK_SIDE) {
-                Pose pose = posesList.get(rnd.nextInt(posesList.size()));
-                poseImageView.setImageResource(pose.getImageId());
-                poseTextView.setText(pose.getTitle());
-            } else {
-                startButton.setEnabled(true);
-            }
-        });
+    @Override
+    public void onGameButtonClicked() {
+        GameFragment gameFragment = GameFragment.newInstance();
+        fragmentManager.beginTransaction().replace(R.id.container, gameFragment).addToBackStack(null).commit();
     }
 }
---
+++ app/src/main/java/com/example/sexposescards/MenuFragment.java
@@ -0,0 +1,51 @@
+package com.example.sexposescards;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.sexposescards.interfaces.OnGameButtonClickListener;
+import com.example.sexposescards.interfaces.OnPreludeButtonClickListener;
+
+public class MenuFragment extends Fragment {
+
+    private Button preludeButton;
+    private Button gameButton;
+
+    private OnPreludeButtonClickListener callbackActivityFromPreludeButtonClicked;
+    private OnGameButtonClickListener callbackActivityFromGameButtonClicked;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        callbackActivityFromPreludeButtonClicked = (OnPreludeButtonClickListener) context;
+        callbackActivityFromGameButtonClicked = (OnGameButtonClickListener) context;
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_menu, container, false);
+
+        initViews(root);
+        preludeButton.setOnClickListener(view -> callbackActivityFromPreludeButtonClicked.onPreludeButtonClicked());
+        gameButton.setOnClickListener(view -> callbackActivityFromGameButtonClicked.onGameButtonClicked());
+
+        return root;
+    }
+
+    @NonNull
+    public static MenuFragment newInstance() {
+        return new MenuFragment();
+    }
+
+    private void initViews(View root) {
+        preludeButton = root.findViewById(R.id.prelude_button);
+        gameButton = root.findViewById(R.id.game_button);
+    }
+}
